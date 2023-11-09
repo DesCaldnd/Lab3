219f9a867bd9c42b11e61518603fa6040e572407
DesCaldnd <ivanlindemann7@gmail.com>
2023-11-09 15:08:09 +0300

    Task 2 without debug

--- T2/main.c
+++ T2/main.c
@@ -18,6 +18,7 @@ typedef double(*vec_norm)(double*, int, void*);
 struct vectors max_vector_norms(int n, int vector_count, int norm_count, ...);
 
 bool d_equal(double lhs, double rhs);
+void matix_vec_prod(double **matrix, double *vec, double* res, int n);
 
 void vector_init(struct vectors* vector);
 bool vector_check(struct vectors vector);
@@ -29,12 +30,52 @@ double norm_1(double* arr, int n, void* data);
 double norm_2(double* arr, int n, void* data);
 double norm_3(double* arr, int n, void* data);
 
+int dyn_alloc_2(int ***data, int n, int m);
+void dyn_free_2(int **data, int n);
+
+
 
 int main()
 {
-    struct vectors res;
+    double vec_1[] = {1, 2, 3};
+    double vec_2[] = {-1, 0, 0.5};
+    double vec_3[] = {5, 7, 3};
+    double vec_4[] = {1, 2, 3};
 
-    vector_free(&res);
+    double p = 2;
+
+    double **matrix;
+
+    if (dyn_alloc_2(&matrix, 3, 3) == -1)
+        return 0;
+
+    matrix[0][0] = 1;
+    matrix[0][1] = 1;
+    matrix[0][2] = 1;
+    matrix[1][0] = 1;
+    matrix[1][1] = 1;
+    matrix[1][2] = 1;
+    matrix[2][0] = 1;
+    matrix[2][1] = 1;
+    matrix[2][2] = 0;
+
+    struct vectors res = max_vector_norms(3, 4, 3, (double *)vec_1, (double *)vec_2, (double *)vec_3, (double *)vec_4, &norm_1, NULL, &norm_2, &p, &norm_3, matrix);
+
+    if (vector_check(res))
+    {
+        for (int i = 0; i < res.size; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                printf("%.2lf ", res.arr[i][j]);
+            }
+            printf("\n");
+        }
+
+        vector_free(&res);
+    }
+
+    dyn_free_2(matrix, 3);
     return 0;
 }
 
@@ -79,7 +120,7 @@ struct vectors max_vector_norms(int n, int vector_count, int norm_count, ...)
 {
     struct vectors res;
     vector_init(&res);
-    if (!vector_check(res) || vector_count < 1 || norm_count < 1)
+    if (!vector_check(res) || vector_count < 1 || norm_count < 1 || n < 1)
         return res;
 
     double **all_vecs = malloc(n * sizeof(double *));
@@ -127,7 +168,6 @@ struct vectors max_vector_norms(int n, int vector_count, int norm_count, ...)
                 ind_written = 1;
             }
         }
-
         for (int j = 0; j < ind_written; ++j)
         {
             vector_push_back(&res, all_vecs[indicies[j]]);
@@ -158,15 +198,81 @@ bool d_equal(double lhs, double rhs)
 
 double norm_1(double* arr, int n, void* data)
 {
+    double max = arr[0];
 
+    for (int i = 1; i < n; ++i)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    return max;
 }
 
 double norm_2(double* arr, int n, void* data)
 {
+    double p = *((double *)data);
+    double accum = 0;
 
+    for (int i = 0; i < n; ++i)
+    {
+        accum += pow(arr[i], p);
+    }
+    return pow(accum, 1 / p);
 }
 
 double norm_3(double* arr, int n, void* data)
 {
+    double **matrix = (double**)data;
+    double *res = malloc(n * sizeof(double ));
+    if (res == NULL)
+        return 0;
 
+    matix_vec_prod(matrix, arr, res, n);
+    double accum = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        accum += res[i] * arr[i];
+    }
+
+    free(res);
+    return sqrt(accum);
+}
+
+void matix_vec_prod(double **matrix, double *vec, double* res, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        double accum = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            accum += matrix[i][j] * vec[j];
+        }
+        res[i] = accum;
+    }
+}
+
+int dyn_alloc_2(int ***data, int n, int m) {
+    *data = malloc(n * sizeof(int *));
+    if (*data == NULL) return -1;
+
+    for (int i = 0; i < n; ++i) {
+        (*data)[i] = malloc(m * sizeof(int));
+        if ((*data)[i] == NULL) {
+            for (int j = 0; j < i; ++j) free((*data)[i]);
+            free(*data);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+void dyn_free_2(int **data, int n) {
+    for (int i = 0; i < n; ++i) {
+        free(data[i]);
+    }
+
+    free(data);
 }
