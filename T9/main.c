d79bca16c6f6c73c8c7df31fde344e589315a193
DesCaldnd <ivanlindemann7@gmail.com>
2023-11-11 00:30:11 +0300

    Task 9 started

---
+++ T9/main.c
@@ -0,0 +1,18 @@
+//
+// Created by Des Caldnd on 11/11/2023.
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct node
+{
+    char* word;
+    int count;
+    struct node *l, *r;
+};
+
+int main(int argc, char *argv[])
+{
+
+}
